10284fb38ed5f5144cd9fa582ab1e71ffb8215cf
Vladimir Andrenkov <v_andrenkov@hotmail.com>
2022-10-17 21:03:37 -0400

    <T> with interface added

---
+++ 16_InterfaceTest_T/InterfaceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    public interface IMyIterfaceTest
+    {
+        public void DoSmth<T>(T param);
+    }
+
+    public class MyInterfaceClass : IMyIterfaceTest
+    {
+        public void DoSmth<T>(T param)
+        {
+            Console.WriteLine("Type is : " + param.GetType().Name);
+        }
+    }
+}
---
+++ 16_InterfaceTest_T/Program.cs
@@ -0,0 +1,6 @@
+﻿using InterfaceTest;
+
+MyInterfaceClass class1 = new MyInterfaceClass();
+class1.DoSmth(23);
+class1.DoSmth(3.62);
+class1.DoSmth("23");
