8bf9c305789716cbb91c80ccb8183d3faa0a2e0e
Vladimir Andrenkov <andrenkov@gmail.com>
2023-03-30 15:28:08 -0400

    ModelExpression added

---
+++ PredicateDemo/PredicateDemo/ModelExpressions.cs
@@ -0,0 +1,27 @@
+﻿using Calabonga.DemoClasses;
+//using Calabonga.PredicatesBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using LinqKit;
+
+namespace PredicateDemo
+{
+    public static class EmployeeModelExpression
+    {
+        public static Expression<Func<Employee, bool>> EmplIsInDepartment(int dptId)
+             => em => em.DepartmentId == dptId;
+
+        public static Expression<Func<Employee, List<Employee>>>? GetCooworkers(string emplName)
+        {
+            var filter = PredicateBuilder.New<Employee>();
+            var emplInDept = People.GetEmployees().Where(x => x.Name == emplName).FirstOrDefault().DepartmentId;
+            filter = filter.And(EmplIsInDepartment(emplInDept));
+
+            return null;// filter;//People.GetEmployees().Where(
+        }
+    }
+}
--- PredicateDemo/PredicateDemo/Program.cs
+++ PredicateDemo/PredicateDemo/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Calabonga.DemoClasses;
-using Calabonga.PredicatesBuilder;
+//using Calabonga.PredicatesBuilder;
+using System.Linq;
+using System.Linq.Expressions;
+using static PredicateDemo.EmployeeModelExpression;
+using LinqKit;
 
-Console.WriteLine("Loading list of People");
+#region Old
+Console.WriteLine("Loading list of People:");
 
 
 var peoples = People.GetPeople();
@@ -27,14 +32,83 @@ var peoples = People.GetPeople();
 //var predicate = PredicateBuilder
 //    .True<Person>()
 //    .And(p => p.Country == Country.Germany);
+
 //the same as above
 var predicate = PredicateBuilder.True<Person>();
-predicate = predicate.And(p => p.Country == Country.Germany);
 
-var qryWhere = predicate.Compile();
-var countriesQry = peoples.Where(qryWhere);
-foreach (Person man in countriesQry)
+var predicateCountry = PredicateBuilder.True<Person>();
+predicateCountry = predicateCountry.And(p => p.Country == Country.Germany);
+predicateCountry = predicateCountry.Or(p => p.Country == Country.China);
+predicate = predicate.And(predicateCountry);
+
+var predicateAge = PredicateBuilder.True<Person>();
+predicateAge = predicateAge.And(p => p.Age > 20 && p.Age < 35);
+
+predicate = predicate.And(p => p.Gender == "M");
+
+
+//var filterWhere = predicate.Compile();
+//var peopleList = peoples.Where(filterWhere);
+//var peopleList = peoples.Where(predicate.Compile());
+var peopleList = peoples.Where((Func<Person, bool>)predicate.Compile());//Func<Person, bool> is a cast
+
+foreach (Person man in peopleList)
 {
-    Console.WriteLine($"{man.Name}");
+    //Console.WriteLine($"name : {man.Name} country : {man.Country} age : {man.Age} gender : {man.Gender}");
 }
+
+//###################################################
+#endregion Old
+
+#region Salary
+Console.WriteLine("--------------------------");
+Console.WriteLine("Loading list of Employee:");
+
+
+var emloyees = People.GetEmployees();
+var departments = People.GetDepartments();
+
+var predicateEmployee = PredicateBuilder.True<Employee>();
+var emplFilterPoor = IsPoor();
+var emplFilterRich = IsRich();
+predicateEmployee = predicateEmployee.And(emplFilterPoor);
+predicateEmployee = predicateEmployee.Or(emplFilterRich);
+
+
+static Expression<Func<Employee, bool>> IsPoor()
+    => em => em.Salary < 100;
+
+static Expression<Func<Employee, bool>> IsRich()
+    => em => em.Salary > 5000;
+
+var emplist = emloyees.Where((Func<Employee, bool>)predicateEmployee.Compile());
+
+foreach (Employee empl in emplist)
+{
+    //Console.WriteLine($"name : {empl.Name} salary : {empl.Salary}");
+}
+//###################################################
+#endregion
+Console.WriteLine("--------------------------");
+Console.WriteLine("Loading department of the employee:");
+
+int dpt = 1; //Global
+string name = "Bob";
+
+var predicateEmployeeDpt = PredicateBuilder.New<Employee>();
+predicateEmployeeDpt = predicateEmployeeDpt.And(EmplIsInDepartment(dpt));
+
+//var dptlist = emloyees.Where(GetCooworkers(name));
+//foreach (Employee empl in dptlist)
+//{
+//    Console.WriteLine($"name : {empl.Name} dpt : {empl.DepartmentId}");
+//}
+
+
+
+
+
+
+
+
 
